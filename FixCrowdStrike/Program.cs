dec9ae145e55de7544abd1166907abf6eb2d74da
Ahmed Chakhoum <a.chakhoum@netcom-group.co>
2024-07-20 22:02:18 +0100

    + First Release

---
+++ FixCrowdStrike/Program.cs
@@ -0,0 +1,48 @@
+﻿Console.WriteLine("""
+                             _            _     _ _
+                   _ __ __ _(_)_ __ __  _| |__ / / |
+                  | '__/ _` | | '_ \\ \/ / '_ \| | |
+                  | | | (_| | | | | |>  <| | | | | |
+                  |_|  \__,_|_|_| |_/_/\_\_| |_|_|_|
+                                                    
+                  made by: https://github.com/rainxh11
+
+                  Remember Kids, it's never too late to ditch windows,
+                  & 3rd party kernel level drivers is a ticking time bomb waiting to go off
+
+                  """);
+
+var drives = DriveInfo.GetDrives()
+                      .Where(drive => drive.DriveType is not DriveType.CDRom and not DriveType.Network);
+foreach (var folder in drives)
+{
+  try
+  {
+    var driverFolder = new DirectoryInfo(Path.Combine(folder.RootDirectory.FullName, "windows", "system32", "drivers", "CrowdStrike "));
+    if (driverFolder.Exists)
+    {
+      Console.WriteLine("Found CrowdStrike folder: {0}", driverFolder.FullName);
+
+      var problematicDrivers = driverFolder.EnumerateFiles("*.sys", SearchOption.AllDirectories)
+                                           .Where(file => file.Name.StartsWith("C-00000291", StringComparison.OrdinalIgnoreCase));
+      foreach (var driverFile in problematicDrivers)
+      {
+        try
+        {
+          driverFile.Delete();
+          Console.WriteLine("Deleted file: {0}", driverFile.FullName);
+        }
+        catch (IOException ex)
+        {
+          Console.WriteLine("Couldn't delete file: {0}, Exception: {1}", driverFile.FullName, ex.Message);
+        }
+      }
+    }
+  }
+  catch
+  {
+  }
+}
+
+Console.WriteLine("Press Any Key to Exit and Shutdown...");
+Console.ReadKey();
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# CrowdStrikeRemover
-CrowdStrike Problematic Windows Driver files Auto Removal Tool
+# CrowdStrike Faulty Driver Remover
+
+Bootable ISO file that automatically finds the faulty CrowdStrike drivers in all system drives and removes them
+
+# How does it work:
+
+Bootable WinPE ISO with custom C# executable that runs on WinPE startup,
+
+which then deletes any driver files that matches the `C-00000291*.sys` file name.
+
+# How to use:
+
+1. Download the [ISO File](https://github.com/rainxh11/CrowdStrikeRemover/releases/download/latest/FixCrowdStrike_PE.iso)
+2. Download an ISO to USB burning tool like [Rufus](https://github.com/pbatard/rufus/releases/download/v4.5/rufus-4.5.exe)
+3. Select and burn `FixCrowdStrike_PE.iso`
+
+![](https://github.com/user-attachments/assets/6bebeb7a-5eb4-47c9-9f6d-58c201f3af8d)
+
+4. Boot the system with the USB Stick
+5. Wait for the Removal Tool to finish and the problematic drivers should be removed
+
+https://github.com/user-attachments/assets/f1414d53-1f8e-448a-bf96-d82150df6a90
